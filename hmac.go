8ef4f4f18374dfc36f5048c350dd6bc2d07425e7
Andreas Jaekle <andreas@jaekle.net>
2014-03-26 14:26:03 +0100

    debug output

--- hmac.go
+++ hmac.go
@@ -83,6 +83,8 @@ func (h *Hmac) ValidateTime(urlp string, secret string) (bool, int) {
 	}
 	newu := h.signRequest(urlp, secret, t)
 	if urlp == newu {
+		fmt.Printf("SH:<%s>\n", urlp)
+		fmt.Printf("IS:<%s>\n", urlu)
 		ts, _ := strconv.Atoi(t.Format(time.RFC850))
 		return true, ts
 	} else {
